447117ed4964efb560f759f829b34249d81dae8d
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-02-19 13:31:14 +0200

    start project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const App = () => {
+
+
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold">
+        Welcom to Allodium
+      </h1>
+    </main>
+  )
+}
+
+export default App
