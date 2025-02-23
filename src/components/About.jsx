483aba1750d131725c4f587efdf004f58ad617e3
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-02-23 16:21:44 +0200

    0.3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,58 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/all';
+import AnimatedTitle from './AnimatedTitle';
 
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#clip',
+        start: 'center center',
+        end: '+=800 center',
+        scrub: 0.5,
+        pinSpacing: true,
+        pin: true,
+      }
+    });
+
+    clipAnimation.to('.mask-clip-path', {
+      width: '100vw',
+      height: '100vh',
+      borderRadius: 0,
+    })
+  })
+
   return (
     <div className="min-h-screen w-screen" id="about">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+        <h2 className="font-general mb-8 text-sm uppercase md:text-[20px]">
           Welcome to Allodium
         </h2>
 
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          Disc<b>o</b>ver the world's l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle containerClass="mt-5 !text-black text-center"
+          title="
+          Disc<b>o</b>ver the world's <br />
+          L<b>a</b>rgest shared adventure"
+         />
 
         <div className="about-subtext">
-          <p>The Game of Games begins-your life, now an epic MMORTS</p>
+          <p>The Game of Games begins-your life, now an epic MMO RTS RPG</p>
 
-          <p>Allodium unites every player from countless games and platforms</p>
+          <p>Allodium unites players from countless games and platforms into one grand adventre</p>
         </div>
       </div>
 
-      
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img className="absolute left-0 top size-full object-cover"
+            src="allodium/Allods_1.jpg" 
+            alt="Background"
+          />
+        </div>
+      </div>
     </div>
   )
 }
