130dfb8a953b06cbc3a5d07df816c20936afbf0e
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-02-24 22:44:21 +0200

    0.4a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,8 @@
 // import clsx from "clsx";
-// import gsap from "gsap";
-// import { useWindowScroll } from "react-use";
+import gsap from "gsap";
+import { useWindowScroll } from "react-use";
 import { useRef, useState, useEffect } from "react";
 import { TiLocationArrow } from "react-icons/ti";
-import { useWindowScroll } from 'react-use';
 import Button from "./Button";
 
 const navItems = ["Ziggurat", "Vault", "Prologue", "About", "Contact"];
@@ -25,9 +24,22 @@ const Navbar = () => {
       navContainerRef.current.classList.remove('floating-nav');
     } else if (currentScrollY > lastScrollY) {
       setIsNavVisible(false);
+      navContainerRef.current.classList.remove('floating-nav');
+    } else if (currentScrollY < lastScrollY) {
+      setIsNavVisible(true);
       navContainerRef.current.classList.add('floating-nav');
     }
+
+    setLastScrollY(currentScrollY);
   }, [currentScrollY]);
+
+  useEffect(() => {
+    gsap.to(navContainerRef, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    })
+  }, [isNavVisible]);
 
   const toggleAudioIndicator = () => {
     setIsAudioPlaying((prev) => !prev);
